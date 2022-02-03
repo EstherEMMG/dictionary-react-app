1697b5b6638b97d68060849195532ce6594b3866
EstherEMMG <96057903+EstherEMMG@users.noreply.github.com>
2022-02-03 11:15:38 +0100

    Install and added Axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [word, setWord] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${word}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateWordChange(event) {
@@ -15,6 +22,8 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
+      <div className="title">Dictionary</div>
+      <div className="question">What word are you looking for?</div>
       <form onSubmit={search}>
         <input type="search" onChange={updateWordChange} />
         <button className="btn btn-primary">Search</button>
